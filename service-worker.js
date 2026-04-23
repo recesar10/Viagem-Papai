5655042512e7d30dd5e1cc01c51cf9982e4369f3
recesar10 <recesar@gmail.com>
2026-04-23 08:35:15 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'roteiro-eua-v4';
+const URLS = ['./', './index.html', './manifest.json', './icon-192-v2.png', './icon-512-v2.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
